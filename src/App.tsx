b2f75923ea04a6bf7615998571488adf16609dd7
vinicius <viniciusgribas@gmail.com>
2025-02-15 18:28:29 -0300

    updates e testes em App

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,26 @@ const App: FC = () => {
     <Router>
       <div className="app">
         <Navbar />
-          {/* Add section IDs here for navigation */}
+        {/* i want a biger space between the navbar and the hero section */}
+        {/* <div className="h-16"></div> */}
+        <div className="container mx-auto">
+          <div className="content">
           <section id="hero">
             <Hero />
           </section>
-
           <section id="about" className="mt-16">
             <About />
           </section>
-
           <section id="projects" className="mt-16">
             <Projects />
           </section>
-
           <section id="contact" className="mt-16">
             <Contact />
           </section>
+          </div>
         </div>
         <footer>{/* Footer */}</footer>
+      </div>
     </Router>
   );
 };
