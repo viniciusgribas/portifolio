d84f68e4157e5fbf2e3ba4a697e320499f18c02e
vinicius <viniciusgribas@gmail.com>
2025-02-15 12:50:47 -0300

    removendo coisas nao uteis

--- etc/refactored_portfolio/src/components/sections/index.ts
+++
@@ -1,4 +0,0 @@
-export { Hero } from './Hero';
-export { About } from './About';
-export { Projects } from './Projects';
-export { Contact } from './Contact'; 
--- refactored_version/src/components/sections/index.ts
+++
@@ -1,6 +0,0 @@
-export { Hero } from './Hero';
-export { About } from './About';
-export { Projects } from './Projects';
-export { Contact } from './Contact'; 
-export { Navbar } from './Navbar'
-export { LanguageChanger } from './LanguageChanger'
